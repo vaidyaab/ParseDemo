3b4fb4d7f3830f2714e3a97a28295cacffa0cb3a
abhijetv <abhijetv@yahoo-inc.com>
2014-07-15 18:36:35 -0700

    Initial Commit

---
+++ parseDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  parseDemo
+//
+//  Created by Abhijeet Vaidya on 7/15/14.
+//  Copyright (c) 2014 yahoo inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
